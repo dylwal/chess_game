f9b69db3bc5e3b6d63ec1af9506190c611c10703
dylwal <dylanwalters00@gmail.com>
2023-09-11 19:10:53 -0400

    Updated code to connect to firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
